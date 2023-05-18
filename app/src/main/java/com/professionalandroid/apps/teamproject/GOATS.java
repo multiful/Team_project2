2497eb74c2a90753de481f356cf718fba5970cee
wondeokyoung <windy970515@gmail.com>
2023-05-18 19:26:00 +0900

    브랜치 추가염!!

--- app/src/main/java/com/professionalandroid/apps/teamproject/GOATS.java
+++ app/src/main/java/com/professionalandroid/apps/teamproject/GOATS.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class GOATofGOAT extends AppCompatActivity {
+public class GOATS extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
