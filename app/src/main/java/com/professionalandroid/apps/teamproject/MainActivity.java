ba3ba2c2cbf8df6279dd579c5456d77bafa79bbc
yoontaemin726 <2003taemin@gmail.com>
2023-05-22 15:36:49 +0900

    quiz 1 추가

--- app/src/main/java/com/professionalandroid/apps/teamproject/MainActivity.java
+++ app/src/main/java/com/professionalandroid/apps/teamproject/MainActivity.java
@@ -2,13 +2,32 @@ package com.professionalandroid.apps.teamproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.mybtn);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myintent = new Intent(getApplicationContext(),quiz1.class);
+                startActivity(myintent);
+            }
+        });
     }
 }
