ef0ce23a58d098e73955b2c753edc4bda56c163e
LeeHeasung <steveen@naver.com>
2023-05-24 21:08:26 +0900

    초기화면부터 게임내 홈화면까지

--- app/src/main/java/com/professionalandroid/apps/teamproject/MainActivity.java
+++ app/src/main/java/com/professionalandroid/apps/teamproject/MainActivity.java
@@ -1,14 +1,13 @@
 package com.professionalandroid.apps.teamproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,13 +18,20 @@ public class MainActivity extends AppCompatActivity {
 
         final EditText userName =(EditText) findViewById(R.id.userName);
         Button startButton = (Button) findViewById(R.id.startButton);
-
+        ImageButton languageButton = (ImageButton) findViewById(R.id.homeImage);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String name =userName.getText().toString();
-                Intent intent =new Intent(getApplicationContext(),startActivity.class);
+                Intent intent =new Intent(getApplicationContext(),storyActivity.class);
                 intent.putExtra("userName",name);
+                startActivity(intent);
+            }
+        });
+        languageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent =new Intent(getApplicationContext(),languageActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/professionalandroid/apps/teamproject/storyActivity.java
+++ app/src/main/java/com/professionalandroid/apps/teamproject/storyActivity.java
@@ -2,11 +2,13 @@ package com.professionalandroid.apps.teamproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class startActivity extends AppCompatActivity {
+public class storyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle bundle){
         super.onCreate(bundle);
@@ -15,6 +17,15 @@ public class startActivity extends AppCompatActivity {
         Intent intent =getIntent();
         String name =intent.getStringExtra("userName");
         userName.setText(name);
+
+        Button skipButton = (Button) findViewById(R.id.skipButton);
+        skipButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(getApplicationContext(),homeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
